a7d78fd83c230b5893e307189d4ca9f9d881cae3
amhasler <amhasler@gmail.com>
2018-02-07 09:36:05 -0500

    omit tokens

--- app/components/about-overlay.js
+++ app/components/about-overlay.js
@@ -3,9 +3,9 @@ import {createClient} from 'contentful';
 
 const client = createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: '4wgubu2ijtof',
+  space: SPACE_ID
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: 'a91d4bc0d6b19ef7bb92c4327fe1d9806b3cc1d49719747650c4f4633d1d459e'
+  accessToken: ACCESS_TOKEN
 });
 
 export default Ember.Component.extend({
